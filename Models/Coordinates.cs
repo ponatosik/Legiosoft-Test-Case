86416ef878bfe757ea662ada173e1cc894926b22
ponatosik <ponatosik@gmail.com>
2024-07-30 17:11:24 +0300

    feat: add models

---
+++ Models/Coordinates.cs
@@ -0,0 +1,7 @@
+﻿namespace Legiosoft_test_case.Models;
+
+public class Coordinates
+{
+	public decimal Latitude { get; set; }
+	public decimal Longitude { get; set; }
+}
---
+++ Models/Transaction.cs
@@ -0,0 +1,11 @@
+﻿namespace Legiosoft_test_case.Models;
+
+public class Transaction
+{
+	public string Id { get; set; }
+	public string Name { get; set; }
+	public string Email { get; set; }
+	public decimal Ammount { get; set; }
+	public Coordinates ClientLocation { get; set; }
+	public DateTime UtcTime { get; set; }
+}

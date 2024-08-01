ed8f043cc48423ce4f239ac261e6feb358246b3d
ponatosik <ponatosik@gmail.com>
2024-08-01 12:55:12 +0300

    fix: missmatch database configuration

--- src/Data/DatabaseConfiguration.cs
+++ src/Data/DatabaseConfiguration.cs
@@ -7,10 +7,8 @@ public static class DatabaseConfiguration
 	public static IServiceCollection UseSqlite(this IServiceCollection services, string dataSource)
 	{
 		var connectionString = $"Data source={dataSource}";
-		var dbConnectionFactory = new SqliteConnectionFactory(dataSource);
+		var dbConnectionFactory = new SqliteConnectionFactory(connectionString);
 
-
-// SQLitePCL.raw.SetProvider(new SQLitePCL.SQLite3Provider_e_sqlite3());
 		services.AddSingleton<IDbConnectionFactory>(dbConnectionFactory);
 		services.AddDbContext<DatabaseContext>(options => options.UseSqlite(connectionString));
 

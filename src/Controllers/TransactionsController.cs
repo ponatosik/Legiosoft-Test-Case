bb23ff038790625993c206dbc833bca07630485f
ponatosik <ponatosik@gmail.com>
2024-08-06 10:49:02 +0300

    feat: get transaction in date range endpoint query options

--- src/Controllers/TransactionsController.cs
+++ src/Controllers/TransactionsController.cs
@@ -8,6 +8,7 @@ namespace Legiosoft_test_case.Controllers;
 [ApiController]
 public class TransactionsController : Controller
 {
+	private readonly ITimezoneService _timezoneService;
 	private readonly ITransactionService _transactionService;
 	private readonly ICsvReader<Transaction> _transactionCsvReader;
 	private readonly IExcelWriter<Transaction> _transactionExcleWriter;
@@ -17,17 +18,37 @@ public class TransactionsController : Controller
 	public TransactionsController(
 		ITransactionService transactionService,
 		ICsvReader<Transaction> transactionCsvReader,
-		IExcelWriter<Transaction> transactionExcleWriter)
+		IExcelWriter<Transaction> transactionExcleWriter,
+		ITimezoneService timezoneService)
 	{
 		_transactionService = transactionService;
 		_transactionCsvReader = transactionCsvReader;
 		_transactionExcleWriter = transactionExcleWriter;
+		_timezoneService = timezoneService;
 	}
 
 	[HttpGet]
-	public async Task<IActionResult> Get()
+	public async Task<IActionResult> Get(
+		[FromQuery(Name = "from")] DateTime? fromDate = null,
+		[FromQuery(Name = "to")] DateTime? toDate = null,
+		[FromQuery(Name = "ianaZone")] string? ianaTimeZoneId = null,
+		[FromQuery(Name = "transactionLocal")] bool useTransactionLocalTime = false)
 	{
-		return Ok(await _transactionService.GetAll());
+		if (fromDate is null && toDate is null)
+		{
+			var transactions = await GetAllTransactions();
+			return Ok(transactions);
+		}
+		if (useTransactionLocalTime)
+		{
+			var transactions = await GetTransactionInLocalDateRange(fromDate, toDate);
+			return Ok(transactions);
+		}
+		else
+		{
+			var transactions = await GetTransactionsInDateRange(fromDate, toDate, ianaTimeZoneId);
+			return Ok(transactions);
+		}
 	}
 
 	[HttpPost]
@@ -54,12 +75,57 @@ public class TransactionsController : Controller
 	}
 
 	[HttpGet("download")]
-	public async Task<IActionResult> Download()
+	public async Task<IActionResult> Download(
+		[FromQuery(Name = "from")] DateTime? fromDate = null,
+		[FromQuery(Name = "to")] DateTime? toDate = null,
+		[FromQuery(Name = "ianaZone")] string? ianaTimeZoneId = null,
+		[FromQuery(Name = "transactionLocal")] bool useTransactionLocalTime = false)
 	{
-		var transactions = await _transactionService.GetAll();
+		if (fromDate is null && toDate is null)
+		{
+			var transactions = await GetAllTransactions();
+			return await TransactionsFile(transactions);
+		}
+		if (useTransactionLocalTime)
+		{
+			var transactions = await GetTransactionInLocalDateRange(fromDate, toDate);
+			return await TransactionsFile(transactions);
+		}
+		else
+		{
+			var transactions = await GetTransactionsInDateRange(fromDate, toDate, ianaTimeZoneId);
+			return await TransactionsFile(transactions);
+		}
+	}
+
+	private async Task<IEnumerable<Transaction>> GetAllTransactions()
+	{
+		return await _transactionService.GetAll();
+	}
+
+	private async Task<IEnumerable<Transaction>> GetTransactionInLocalDateRange(DateTime? fromDate, DateTime? toDate)
+	{
+		var from = fromDate ?? DateTime.MinValue;
+		var to = toDate ?? DateTime.MinValue;
+		return await _transactionService.GetInTransactionLocalDateRange(from, to);
+	}
+
+	private async Task<IEnumerable<Transaction>> GetTransactionsInDateRange(
+		DateTime? fromDate,
+		DateTime? toDate,
+		string? ianaTimeZoneId)
+	{
+		var timeZoneId = ianaTimeZoneId ?? "Etc/GMT";
+		TimeSpan timeZoneOffset = _timezoneService.GetTimeZoneOffset(timeZoneId) ?? TimeSpan.Zero;
+		var from = fromDate?.Add(timeZoneOffset) ?? DateTime.MinValue;
+		var to = toDate?.Add(timeZoneOffset) ?? DateTime.MaxValue;
+		return await _transactionService.GetInDateRange(from, to);
+	}
+
+	private async Task<FileContentResult> TransactionsFile(IEnumerable<Transaction> transactions)
+	{
 		var fileBytes = await _transactionExcleWriter.WriteBytes(transactions);
 		var file = new FileContentResult(fileBytes, EXCEL_MIME_TYPE);
-
 		var timeStamp = DateTime.UtcNow.ToFileTime();
 		file.FileDownloadName = $"transactions_{timeStamp}.xlsx";
 		return file;
